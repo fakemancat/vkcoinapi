ebf410a3fee8d301a62fa6714c674506d8f9e663
fakemancat <bryuhanovdima@yandex.ru>
2019-04-17 22:22:18 +0500

    Фикс пустого файла

---
+++ examples/simpleBot.js
@@ -0,0 +1,27 @@
+const VKCOINAPI = require('vkcoinapi'); // Импорт модуля
+const vkcoin = new VKCOINAPI({
+    key: 'Тут ключ',
+    userId: 123, // Тут ваш ID вк
+    token: 'Тут токен'
+});
+
+const { VK } = require('vk-io'); // Импорт модуля для бота
+const vk = new VK(); // Новый экземпляр 
+
+vk.setOptions({ // Устанавливаем опции
+    pollingGroupId: 123, // Тут ID группы
+    token: 'Тут токен бота (группы)'
+});
+
+vk.updates.on(['new_message'], async(msg) => { // Прослушка новых сообщений
+    if (msg.isOutbox) return; // Если исходящее, то возвращаем
+
+    if (msg.text === 'мой баланс') { // Если входящее сообщение будет в точности равно 'мой баланс', то...
+        const result = await vkcoin.getBalance([ msg.senderId ]); // Получаем баланс отправителя
+        const coins = vkcoin.formatCoins(result.response[msg.senderId]); // Делаем его читабельным
+
+        return msg.send(`Ваши коины: ${coins}`); // Отправляем сообщение
+    }
+});
+
+vk.updates.startPolling(); // Старт прослушивания
