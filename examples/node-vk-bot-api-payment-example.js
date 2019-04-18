bef8a915173ad6597036137412715fbfe8c6bb9b
nitrojs <n1trojscoding@mail.ru>
2019-04-18 21:05:53 +0300

    Добавление примеров для sendPayment

---
+++ examples/node-vk-bot-api-payment-example.js
@@ -0,0 +1,23 @@
+const VKCOINAPI = require('node-vkcoinapi'); // Импорт модуля
+const vkcoin = new VKCOINAPI({
+    key: 'Тут ключ',
+    userId: 123, // Тут ваш ID вк
+    token: 'Тут токен'
+});
+
+const VK = require('node-vk-bot-api'); // Импорт модуля для бота
+const bot = new VK(); // Новый экземпляр
+
+bot.use((ctx, next) => { // Прослушка для каждого события
+    if (ctx.message.from_id < 0) return; // Если от ботов, то ничего не делаем
+
+    return next(); // А если всё норм, то идём дальше
+});
+
+bot.command(/дайте коинов/i, async (ctx) => { // Прослушка новых сообщений "дайте коинов"
+    await vkcoin.sendPayment(ctx.message.from_id, 1000000); // Отправляем 1000 коинов пользователю
+
+    return ctx.reply('Мы отправили вам 1000 коинов, можете проверить!'); // Отправляем сообщение
+});
+
+bot.startPolling(); // Старт прослушивания
---
+++ examples/vk-io-payment-example.js
@@ -0,0 +1,26 @@
+const VKCOINAPI = require('node-vkcoinapi'); // Импорт модуля
+const vkcoin = new VKCOINAPI({
+    key: 'Тут ключ',
+    userId: 123, // Тут ваш ID вк
+    token: 'Тут токен'
+});
+
+const { VK } = require('vk-io'); // Импорт модуля для бота
+const vk = new VK(); // Новый экземпляр 
+
+vk.setOptions({ // Устанавливаем опции
+    pollingGroupId: 123, // Тут ID группы
+    token: 'Тут токен бота (группы)'
+});
+
+vk.updates.on(['new_message'], async(msg) => { // Прослушка новых сообщений
+    if (msg.isOutbox) return; // Если исходящее, то возвращаем
+
+    if (msg.text === 'дайте коинов') { // Если входящее сообщение будет в точности равно 'дайте коинов', то...
+        await vkcoin.sendPayment(msg.senderId, 1000000); // Отправляем 1000 коинов пользователю
+
+        return msg.send('Мы отправили вам 1000 коинов, можете проверить!'); // Отправляем сообщение
+    }
+});
+
+vk.updates.startPolling(); // Старт прослушивания
